5e7b417ed7c2b7574da0814b2bb5e7ed7ce301c2
Keiran Paster <keiran@me.com>
2022-10-18 20:28:50 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,9 @@
 from setuptools import setup
 
-try:
-    # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:
-    # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
-
-def load_requirements(fname):
-    reqs = parse_requirements(fname, session="test")
-    return [str(ir.req) for ir in reqs]
-
-
 setup(
     name='return_transforms',
     version='1.0',
     description='',
     author='Keiran Paster',
     packages=['return_transforms'],
-    install_requires=load_requirements('requirements.txt'),
 )
